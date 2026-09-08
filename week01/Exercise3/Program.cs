515170622ad50d8401ecaa11aa6cbddeda2a6212
gabriel-reina-montaldo <greinamontaldo@byupathway.edu>
2026-09-08 19:43:09 -0300

    week 01 exercise 3 added

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,25 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
-        string magicNumberString = Console.ReadLine();
-        int magicNumber = int.Parse(magicNumberString);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
-        Console.WriteLine("What is your guess? ");
-        string guessString = Console.ReadLine();
-        int guess = int.Parse(guessString);
+        int guess = -1;
 
-        if (guess < magicNumber)
+        while (guess != magicNumber)
         {
-            Console.WriteLine("Higher!");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower!");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
