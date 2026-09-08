88292fbf53612f947cf84fed7b236f05a3f6749c
gabriel-reina-montaldo <greinamontaldo@byupathway.edu>
2026-09-08 19:21:09 -0300

    week 01 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? ");
+        string magicNumberString = Console.ReadLine();
+        int magicNumber = int.Parse(magicNumberString);
+
+        Console.WriteLine("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
