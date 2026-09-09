7f457652d1c937e95a1b1678296726231bc32c40
gabriel-reina-montaldo <greinamontaldo@byupathway.edu>
2026-09-08 21:48:00 -0300

    week 01 ex 5 added

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        DisplayWelcome();
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        string userName = PromptUserName();
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+
+        int userNumber = PromptUserNumber();
+
+        static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+        int squareNumber = SquareNumber(userNumber);
+
+        static void DisplayResult(string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared}.");
+        }
+
+        DisplayResult(userName, squareNumber);
     }
 }
